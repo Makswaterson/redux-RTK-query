f9357de526c5afa9c9d71dc745ab7d39926e483c
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-04-17 12:29:31 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Makswaterson.github.io/redux-RTK-query',
   plugins: [react()],
   server: {
     open: '/index.html',
